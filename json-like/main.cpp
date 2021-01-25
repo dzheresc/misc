eaf165fd3aa24514f576abbbc1306ad40f8fa148
Denis Zhereschin <denis.zhereschin@r3.com>
2021-01-25 10:56:04 +0000

    clang-format

--- json-like/main.cpp
+++ json-like/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <memory>
 #include <optional>
 #include <string>
 #include <variant>
 #include <vector>
-#include <memory>
 
 using namespace std;
 
@@ -18,83 +18,78 @@ using namespace std;
 class doc_t;
 class kv_t {
 public:
-    string key;
-    variant<bool,doc_t*> val;
+  string key;
+  variant<bool, doc_t *> val;
 };
 
 class doc_t {
 
-    bool is_valid(int& flags, doc_t* doc){
-        for(auto kv : doc->data){
-            if (holds_alternative<doc_t*>(kv.val)){
-                if(!is_valid(flags, get<doc_t*>(kv.val))){
-                    return false;
-                }
-            }
-            else { // holds_alternative<bool>
-                // bit0 -> false, bit4 -> true
-                // 0x11 -> error
-                flags |= get<bool>(kv.val) ? 0x10 : 0x01;
-                if (flags == 0x11)
-                    return false;
-            }
+  bool is_valid(int &flags, doc_t *doc) {
+    for (auto kv : doc->data) {
+      if (holds_alternative<doc_t *>(kv.val)) {
+        if (!is_valid(flags, get<doc_t *>(kv.val))) {
+          return false;
         }
-        return true;
+      } else { // holds_alternative<bool>
+        // bit0 -> false, bit4 -> true
+        // 0x11 -> error
+        flags |= get<bool>(kv.val) ? 0x10 : 0x01;
+        if (flags == 0x11)
+          return false;
+      }
     }
+    return true;
+  }
 
 public:
-    vector<kv_t> data;
+  vector<kv_t> data;
 
-    doc_t(initializer_list<kv_t> list){
-        for (auto kv : list) {
-            data.push_back(kv);
-        }
+  doc_t(initializer_list<kv_t> list) {
+    for (auto kv : list) {
+      data.push_back(kv);
     }
+  }
 
-    ~doc_t(){
-        for (auto kv : data) {
-	   if (holds_alternative<doc_t*>(kv.val))
-               delete get<doc_t*>(kv.val);
-        }
+  ~doc_t() {
+    for (auto kv : data) {
+      if (holds_alternative<doc_t *>(kv.val))
+        delete get<doc_t *>(kv.val);
     }
+  }
 
-    bool is_valid(){
-        int flags{0};
-        return is_valid(flags, this);
-    }
+  bool is_valid() {
+    int flags{0};
+    return is_valid(flags, this);
+  }
 };
 
-class assertion_exception: public exception {
-    const string& error;
+class assertion_exception : public exception {
+  const string &error;
+
 public:
-    assertion_exception(const string& error)
-        :error(error){
-    }
+  assertion_exception(const string &error) : error(error) {}
 
-    const char * what() const noexcept override {
-        return error.c_str();
-    }
+  const char *what() const noexcept override { return error.c_str(); }
 };
 
-void assert(bool expression, const string& error){
-    if (!expression) {
-        throw assertion_exception(error);
-    }
+void assert(bool expression, const string &error) {
+  if (!expression) {
+    throw assertion_exception(error);
+  }
 }
 
 int main() {
-    auto make_doc = [](bool a, bool c){
-      return unique_ptr<doc_t>(new doc_t
-      {
+  auto make_doc = [](bool a, bool c) {
+    return unique_ptr<doc_t>(new doc_t{
         {"a", a},
-        {"b", new doc_t { {"c", c} }},
-      });
-    };
+        {"b", new doc_t{{"c", c}}},
+    });
+  };
 
-    assert(make_doc(true,true)->is_valid(), "all true");
-    assert(make_doc(false,false)->is_valid(), "all false");
-    assert(make_doc(true,false)->is_valid() == false, "disallow mix");
+  assert(make_doc(true, true)->is_valid(), "all true");
+  assert(make_doc(false, false)->is_valid(), "all false");
+  assert(make_doc(true, false)->is_valid() == false, "disallow mix");
 
-    cout << "ok\n";
-    return 0;
+  cout << "ok\n";
+  return 0;
 }
